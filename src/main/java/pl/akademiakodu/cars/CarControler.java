cf92847436037c91a94088b9634b1649dafe630a
Dawid Czartoszewski <dawid.czartoszewski@gmail.com>
2018-07-16 11:13:52 +0200

    second commit

---
+++ src/main/java/pl/akademiakodu/cars/CarControler.java
@@ -0,0 +1,30 @@
+package pl.akademiakodu.cars;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+
+@Controller
+public class CarControler {
+
+    @GetMapping("/")
+    public String addCar() {
+        return "add";
+    }
+
+    @GetMapping("/add")
+    public String add(@ModelAttribute
+                              Car car, ModelMap modelMap) {
+        modelMap.put("car", car);
+        return "show";
+    }
+
+    @GetMapping("/all")
+    public String findAll(ModelMap modelMap) {
+        modelMap.put("cars", Car.findAll());
+        return "all";
+    }
+}
+
